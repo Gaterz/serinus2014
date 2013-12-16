89978819f5fd23bda454858c9268341dfc0786b9
gaterz <julien.maurat@free.fr>
2013-12-16 20:24:48 +0100

    +Asservissement_HL : Ajout des fonction de mode du correcteur(P,PI,PD)

--- Functions/Asservissement_HL.c
+++ Functions/Asservissement_HL.c
@@ -34,16 +34,24 @@ valeurs de codeuses envoyées
 #define P_PID_ANGLE 1
 #define I_PID_ANGLE 0
 #define D_PID_ANGLE 0
-///////////gloabl_vars/////////////////////////////////////////////////////
-signed long Asserv_Cons_distance = 10;
-signed long Asserv_Cons_angle = 10;
-signed long Asserv_Integrale_Distance = 10;
-signed long Asserv_Integrale_angle = 10;
-signed long Asserv_Derivee_Distance = 10;
-signed long Asserv_Derivee_angle = 10;
+#define MODE_P 0
+#define MODE_PI 1
+#define MODE_PD 2
+///////////global_vars/////////////////////////////////////////////////////
+signed long Asserv_Cons_distance = 0;
+signed long Asserv_Cons_angle = 0;
+///////////static_vars/////////////////////////////////////////////////////
+static int Asserv_mode=0;
+static signed long Asserv_Integrale_Distance = 0;
+static signed long Asserv_Integrale_angle = 0;
+static signed long Asserv_Derivee_Distance = 0;
+static signed long Asserv_Derivee_angle = 0;
 ///////////////////////////////////////////////////////////////////////////
 void Gestion_Asserv_HL(signed long Tick_droit,signed long Tick_gauche,signed long* Cons_droite,signed long* Cons_gauche)
 {
+	//Vars////////////////////////////////////////////////////////////
+	signed long delta_distance=0;
+	signed long delta_angle=0;
 	//Moyennage////////////////////////////////////////////////////////////
 	signed long Moyenne = (Tick_droit+Tick_gauche)/2;
 
@@ -55,27 +63,59 @@ void Gestion_Asserv_HL(signed long Tick_droit,signed long Tick_gauche,signed lon
 	signed long Erreur_angle 		=Asserv_Cons_angle		-	Difference;
 
 	//Intégration//////////////////////////////////////////////////////////
-	Asserv_Integrale_Distance	+=Erreur_distance;
-	Asserv_Integrale_angle		+=Erreur_angle;
+	if(Asserv_mode==MODE_PI)
+	{
+		Asserv_Integrale_Distance	+=Erreur_distance;
+		Asserv_Integrale_angle		+=Erreur_angle;
+	}
 
 	//Dérivation///////////////////////////////////////////////////////////
-	//..distance//
-	signed long delta_distance		=Erreur_distance	-	Asserv_Derivee_Distance;
-	Asserv_Derivee_Distance	=Erreur_distance;
-	//..angle//
-	signed long delta_angle		=Erreur_angle		-	Asserv_Derivee_angle;
-	Asserv_Derivee_angle	=Erreur_angle;
-
+	if(Asserv_mode==MODE_PD)
+	{
+		//..distance//
+		delta_distance	=Erreur_distance	-	Asserv_Derivee_Distance;
+		Asserv_Derivee_Distance	=Erreur_distance;
+		//..angle//
+		delta_angle		=Erreur_angle		-	Asserv_Derivee_angle;
+		Asserv_Derivee_angle	=Erreur_angle;
+	}
 	//Application des coefficients/////////////////////////////////////////
-	signed long ordre_distance	=	Erreur_distance	* P_PID_DISTANCE			+
-							Asserv_Integrale_Distance * I_PID_DISTANCE 	+
+	signed long ordre_distance	=	Erreur_distance	* P_PID_DISTANCE	+
+							Asserv_Integrale_Distance * I_PID_DISTANCE 	-
 							delta_distance * D_PID_DISTANCE;
 
-	signed long ordre_angle	=	Erreur_angle * P_PID_ANGLE				+
-							Asserv_Integrale_angle * I_PID_ANGLE 	+
+	signed long ordre_angle		=	Erreur_angle * P_PID_ANGLE			+
+							Asserv_Integrale_angle * I_PID_ANGLE 		-
 							delta_angle * D_PID_ANGLE;
 
 	//Calcul des consignes/////////////////////////////////////////////////
 	*Cons_droite=ordre_distance+ordre_angle;
 	*Cons_gauche=ordre_distance-ordre_angle;
+}
+void Mode_Asserv(int mode)
+{
+	switch(mode)
+	{
+		case MODE_P:
+			Asserv_Reset_Integrateur();
+			Asserv_Reset_Derivateur();
+		break;
+		case MODE_PI:
+			Asserv_Reset_Derivateur();
+		break;
+		case MODE_PD:
+			Asserv_Reset_Integrateur();
+		break;
+	}
+	Asserv_mode=mode;
+}
+void Asserv_Reset_Integrateur(void)
+{
+	Asserv_Integrale_Distance = 0;
+	Asserv_Integrale_angle = 0;
+}
+void Asserv_Reset_Derivateur(void)
+{
+	Asserv_Derivee_Distance = 0;
+	Asserv_Derivee_angle = 0;
 }
--- Headers/Asservissement_HL.h
+++ Headers/Asservissement_HL.h
@@ -3,5 +3,6 @@
 extern signed long Asserv_Cons_distance;
 extern signed long Asserv_Cons_angle;
 void Gestion_Asserv_HL(long Tick_droit,long Tick_gauche,long* Cons_droite,long* Cons_gauche);
-
+void Asserv_Reset_Integrateur(void);
+void Asserv_Reset_Derivateur(void);
 #endif /* ASSERVISSEMENT_HL_H_ */
