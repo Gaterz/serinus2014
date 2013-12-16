9b4d85edb7c72081f762312f055599564670a27d
gaterz <julien.maurat@free.fr>
2013-12-16 23:25:58 +0100

    +const.h : ajout des constantes

--- all_head.h
+++ all_head.h
@@ -2,4 +2,5 @@
 #define ALL_HEAD_H_
 #include "Headers/Low_LVL.h"
 #include "Headers/Functions.h"
+#include "const.h"
 #endif /* ALL_HEAD_H_ */
---
+++ const.h
@@ -0,0 +1,7 @@
+#ifndef CONST_H_
+#define CONST_H_
+#define POS_X_INITIALLE 0
+#define POS_Y_INITIALLE 0
+#define ANGLE_INITIAL 0
+#define ENTRAXE_TICK 1
+#endif /* CONST_H_ */
