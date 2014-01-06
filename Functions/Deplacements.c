d861b488c4dd8f4d1741e3379a7054d13fb9778b
gaterz <julien.maurat@free.fr>
2014-01-06 08:42:49 +0100

    %Deplacements : adaptation des paramètres

--- Functions/Deplacements.c
+++ Functions/Deplacements.c
@@ -6,14 +6,10 @@ Constantes :
 Fonctions :
 **************************************************************************************/
 #include "../all_head.h"
-void get_linear_coord(signed long x_dest,signed long y_dest,signed long x_source,long double angle_source,signed long y_source,signed long* distance,long double angle)
+void get_linear_coord(signed long x_dest,signed long y_dest,signed long x_source,signed long y_source,signed long* distance,long double* angle)
 {
 	signed long delta_x=x_dest-x_source;
 	signed long delta_y=y_dest-y_source;
 	*distance=sqrt(delta_x*delta_x+delta_y*delta_y);
-	if(*distance > MAX_CONSIGNE_DISTANCE)
-	{
-		*distance = MAX_CONSIGNE_DISTANCE;
-	}
 
 }
--- const.h
+++ const.h
@@ -4,5 +4,4 @@
 #define POS_Y_INITIALLE 0
 #define ANGLE_INITIAL 0
 #define ENTRAXE_TICK 1
-#define MAX_CONSIGNE_DISTANCE 500
 #endif /* CONST_H_ */
