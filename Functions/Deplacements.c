fffaafcc87f8576e62fa790dc2f5687c89842834
gaterz <julien.maurat@free.fr>
2014-01-20 10:04:16 +0100

    %Deplacements : merge ok, a faire : move to

--- Functions/Deplacements.c
+++ Functions/Deplacements.c
@@ -7,10 +7,16 @@ Fonctions :
 **************************************************************************************/
 #include "../all_head.h"
 #include <math.h>
+//static int
 void get_linear_coord(signed long x_dest,signed long y_dest,signed long x_source,signed long y_source,signed long* distance,long double* angle)
 {
 	signed long delta_x=x_dest-x_source;
 	signed long delta_y=y_dest-y_source;
 	*distance=sqrt(delta_x*delta_x+delta_y*delta_y);
+	*angle=atan2(delta_x,delta_y);
+}
+
+void move_to(signed long x_dest,signed long y_dest)
+{
 
 }
