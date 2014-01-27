777965a113d08270f185fdee37c83c9127b3fa26
gaterz <julien.maurat@free.fr>
2014-01-27 09:09:08 +0100

    +Deplacements : fonction getDeplacement_state basique %Deplacements.h : constantes d'état pour le déplacement %const.h : ajout des seuil d'angles et de distance pour le deplacements

--- Functions/Deplacements.c
+++ Functions/Deplacements.c
@@ -15,8 +15,39 @@ void get_linear_coord(signed long x_dest,signed long y_dest,signed long x_source
 	*distance=sqrt(delta_x*delta_x+delta_y*delta_y);
 	*angle=atan2(delta_x,delta_y);
 }
+/*
+signed long Asserv_Cons_distance = 0;
+signed long Asserv_Cons_angle = 0;
 
+///////////static_vars/////////////////////////////////////////////////////
+static int Asserv_mode=0;
+*/
+static signed int phase_deplacement=DEPLACEMENT_ARRET;
 void move_to(signed long x_dest,signed long y_dest)
 {
+	signed long distance;
+	long double angle;
+	get_linear_coord(x_dest,y_dest,Odo_pos_x,Odo_pos_x,&distance,&angle);
+	Mode_Asserv(MODE_PD);
+	getDeplacement_state(distance,angle);
+}
 
+signed int getDeplacement_state(signed long distance,long double angle)
+{
+	signed int state=DEPLACEMENT_ARRET;
+	if(angle < THRESHOLD_ANGLE)
+	{
+		if(distance < THRESHOLD_DISTANCE)
+		{
+			state=DEPLACEMENT_ARRET;
+		}
+		else
+		{
+			state=DEPLACEMENT_AVANCE;
+		}
+	}
+	else
+	{
+		state=DEPLACEMENT_ROTATION;
+	}
 }
--- Headers/Deplacements.h
+++ Headers/Deplacements.h
@@ -1,5 +1,8 @@
 #ifndef DEPLACEMENTS_H_
 #define DEPLACEMENTS_H_
+#define DEPLACEMENT_ARRET 0
+#define DEPLACEMENT_ROTATION 1
+#define DEPLACEMENT_AVANCE 2
 void get_linear_coord(signed long x_dest,signed long y_dest,signed long x_source,signed long y_source,signed long* distance,long double* angle);
 
 #endif /* DEPLACEMENTS_H_ */
