a039a09e1496ea33b524c93cb3cf29a6809a9373
gaterz <julien.maurat@free.fr>
2014-05-19 11:53:15 +0200

    %can_HL : switch sur message id

--- Functions/can_HL.c
+++ Functions/can_HL.c
@@ -9,16 +9,20 @@ Fonctions :
 void Routine_CAN()
 {
 	if(CANReceiveMessage(&Message_R)==1)
-			{
-				if(Message_R.stdIdf==ID_MSG_CAN_SONARS)
-				{
-					Dist_Sonard0=(Message_R.dataL&0xFF)+(Message_R.dataL&0xFF00);
-					Dist_Sonard1=(Message_R.dataL>>16&0xFF)+(Message_R.dataL>>16&0xFF00);
-					Dist_Sonard2=(Message_R.dataH&0xFF)+(Message_R.dataH&0xFF00);
-					Dist_Sonard3=(Message_R.dataH>>16&0xFF)+(Message_R.dataH>>16&0xFF00);
-					//Message_R.stdIdf=2;
-					//CANTransmitMessage(&Message_R);
-				}
+	{
+		switch(Message_R.stdIdf)
+		{
+		case ID_MSG_CAN_SONARS :
+			Dist_Sonard0=(Message_R.dataL&0xFF)+(Message_R.dataL&0xFF00);
+			Dist_Sonard1=(Message_R.dataL>>16&0xFF)+(Message_R.dataL>>16&0xFF00);
+			Dist_Sonard2=(Message_R.dataH&0xFF)+(Message_R.dataH&0xFF00);
+			Dist_Sonard3=(Message_R.dataH>>16&0xFF)+(Message_R.dataH>>16&0xFF00);
+			//Message_R.stdIdf=2;
+			//CANTransmitMessage(&Message_R);
+			break;
+		case ID_MSG_CAN_BUTTON :
 
-			}
+			break;
+		}
+	}
 }
